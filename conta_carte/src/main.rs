7b7e3b161483775d2d44405756ab346f5e629e17
CiroBurro <filobaglioni.06@gmail.com>
2024-10-04 16:11:41 +0200

    progetto conta carte

---
+++ conta_carte/src/main.rs
@@ -0,0 +1,17 @@
+enum  Carta {
+    Asso(String),
+    Uno(String), 
+    Due(String),
+    Tre(String),
+    Quattro(String),
+    Cinque(String),
+    Sei(String),
+    Sette(String),
+    Otto(String),
+    Nove(String),
+    Dieci(String),
+    Jack(String),
+    Donna(String),
+    Re(String), 
+
+}
