431c36c6ce00ecab0de9a9c6e56395d0913fe9da
filobaglioni.06 <filobaglioni.06@gmail.com>
2024-10-16 11:50:32 +0000

    rimozione "alcolatrice-test

--- calcolatrice_test/src/main.rs
+++
@@ -1,8 +0,0 @@
-use meval::Expr;
-
-fn main() {
-    let testo:&str = "(5 + 7*3 - 4)/2";
-    let espressione: Expr = testo.parse().unwrap();
-    let risultato:f64 = espressione.eval().unwrap();
-    println!("{}", risultato)
-}
